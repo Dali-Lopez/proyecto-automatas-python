bdae170830f60031ab7a693445b7860272a88fbe
Dali-Lopez <dalilopez-martinezt@hotmail.com>
2021-10-29 18:53:12 -0500

    a

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import graphviz
 #Generando el diagrama
 f = graphviz.Digraph('finite_state_machine', filename='diagrama.png')
 f.attr(rankdir='LR',size='800')
+
+nodo_inicial = {'a'}
 a='Nodo inicial'
 #dibujar nodo inicial
 f.node( name='', shape='point')
 f.node(a,shape='circle')
-f.edge( '',a,'',shape='point')
+f.edge( '',nodo_inicial,'',shape='point')
 
 vector = {'a':2,'v':1}
 print(vector.get('a'))
