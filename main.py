5f2ed29c5bc0c570587b6839eb15ff9ae9c4efba
Dali-Lopez <dalilopez-martinezt@hotmail.com>
2021-10-29 18:50:17 -0500

    Diagrama inicial

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import graphviz
+#Generando el diagrama
+f = graphviz.Digraph('finite_state_machine', filename='diagrama.png')
+f.attr(rankdir='LR',size='800')
+a='Nodo inicial'
+#dibujar nodo inicial
+f.node( name='', shape='point')
+f.node(a,shape='circle')
+f.edge( '',a,'',shape='point')
+
 vector = {'a':2,'v':1}
 print(vector.get('a'))
 print(type(vector))
+#Generar el diagrama
+f.render()
